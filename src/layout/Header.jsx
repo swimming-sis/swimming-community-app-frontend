773efebbc16cbd9bcb5f7851ee7fd0298ae9a617
HANCHU <hyunjulee@HANCHU.local>
2023-09-09 00:27:16 +0900

    fit Header z-index 및 header tag로 수정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ import Search from '@/components/Icon/Search';
 import X from '@/components/Icon/X';
 import propTypes from 'prop-types';
 
-function Header({ condition = 'default', content = '', noEdit=false}) {
+function Header({ condition = 'search', content = '', noEdit=false}) {
   const [value, setValue] = useState("");
 
 
@@ -23,39 +23,41 @@ function Header({ condition = 'default', content = '', noEdit=false}) {
   return (
     <>
       {condition==='default' &&
-      <div className={`flex ${noEdit&&'justify-between'} items-center h-10 font-semibold mt-3 font-pretendard`}>
+      <header className={`flex ${noEdit&&'justify-between'} items-center h-10 font-semibold mt-3 font-pretendard z-50`}>
         <Back />
         <p className={`text-secondary ${noEdit || 'flex-grow text-center mr-9'}`}>{content}</p>
         {noEdit&&<Pencil />}
-      </div> }
+      </header> }
       {condition==='search' &&
-      <form className='flex flex-col h-10 font-semibold mt-3 relative'>
-        <label 
-        htmlFor="flex-grow searchSwimmingPool" 
-        className=' text-secondary mb-2 ml-4'
-        tabIndex={0}>전국 수영장 검색하기</label>
-        <input 
-        type="search" 
-        name="searchSwimmingPool" 
-        value={value} 
-        id="searchSwimmingPool" 
-        onChange={handleChange}
-        className='w-[calc(100%-20px)] p-2 rounded-lg mx-auto border border-secondary'/>
-        <button 
-        type="submit" 
-        onClick={handleSearch}
-        className='absolute 
-        right-7 top-[39px]'>
-        <Search/>
-        </button>
-        {value && <button 
-        type="button" 
-        className='absolute right-16 top-[48px] btn-clear'
-        onClick={handleTextClear}
-        >
-          <X/>
-        </button>}
-      </form> }
+      <header className="z-50">
+        <form className='flex flex-col h-10 font-semibold mt-3 relative'>
+          <label
+          htmlFor="flex-grow searchSwimmingPool"
+          className=' text-secondary mb-2 ml-4'
+          tabIndex={0}>전국 수영장 검색하기</label>
+          <input
+          type="search"
+          name="searchSwimmingPool"
+          value={value}
+          id="searchSwimmingPool"
+          onChange={handleChange}
+          className='w-[calc(100%-20px)] p-2 rounded-lg mx-auto border border-secondary'/>
+          <button
+          type="submit"
+          onClick={handleSearch}
+          className='absolute
+          right-7 top-[39px]'>
+          <Search/>
+          </button>
+          {value && <button
+          type="button"
+          className='absolute right-16 top-[48px] btn-clear'
+          onClick={handleTextClear}
+          >
+            <X/>
+          </button>}
+        </form>
+      </header> }
     </>
   );
 }
